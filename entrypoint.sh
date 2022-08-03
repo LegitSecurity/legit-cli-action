a3c8eceb23c819472f07edfe64c0ff7b1428bfdf
shay-legit <shay@legitsecurity.com>
2022-08-03 19:55:25 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-command=$1
-from=$2
-to=$3
-min_severity=$3
-
-LEGIT_API_TOKEN=$token
-
-/legit $command --from=$from --to=$to --min-severity=$min_severity
+/legit $COMMAND --from=$FROM_COMMIT --to=$TO_COMMIT --min-severity=$MIN_SEV
