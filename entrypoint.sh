62633085ffe726a3894be35fd2089f400ec054ce
shay-legit <shay@legitsecurity.com>
2022-08-03 21:14:14 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git config --global --add safe.directory /github/workspace
 echo $FROM_COMMIT
-echo TO_COMMIT
+echo $TO_COMMIT
 /legit "$COMMAND" --from="$FROM_COMMIT" --to="$TO_COMMIT" --min-severity="$MIN_SEV"
